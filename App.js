7e933e0f72354fd2b5e7c4ba5db9f91d6e2dadb5
KattachaithanyaKumar <20835a0504@gniindia.org>
2023-11-06 14:55:25 +0530

    initial commit

---
+++ App.js
@@ -0,0 +1,22 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { StatusBar } from "expo-status-bar";
+
+const Stack = createNativeStackNavigator();
+
+//screens
+import Login from "./Screens/Login";
+import Signup from "./Screens/Signup";
+import Home from "./Screens/Home";
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Signup" component={Signup} />
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
